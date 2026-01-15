97939a5f545de9094dd8e9b0d984bb697b555cff
lxgr-linux <lxgr@protonmail.com>
2026-01-15 11:40:56 +0100

    feat: macronaut compat

--- generation/lang.go
+++ generation/lang.go
@@ -2,7 +2,7 @@ package generation
 
 var mapping = map[string]string{
 	"java": "java", "py": "Python", "xml": "XML", "sh": "bash",
-	"go": "go", "js": "JavaScript", "ts": "JavaScript", "kt": "Kotlin", "kts": "Kotlin",
+	"go": "go", "js": "JavaScript", "ts": "JavaScript", "kt": "Kotlin", "kts": "Kotlin", "rs": "Rust",
 }
 
 func Map(format string) string {
